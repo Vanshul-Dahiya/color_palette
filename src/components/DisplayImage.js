75ba9f655a75bcedf5138df4ce82f2f86d6a5cd0
Vanshul <vanshuldahiya2002@gmail.com>
2023-04-30 10:46:23 +0530

    completed

--- src/components/DisplayImage.js
+++ src/components/DisplayImage.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import styles from "../styles/Home.module.css";
+import ListItem from "./ListItem";
 
 const DisplayImage = ({ uploadedImage, colorPalette }) => {
+  const toHex = (rgb) => {
+    let hex = Number(rgb).toString(16);
+    if (hex.length < 2) {
+      hex = "0" + hex;
+    }
+    return hex;
+  };
   return (
     <div className={styles.content}>
       <div className="image">
@@ -11,6 +19,17 @@ const DisplayImage = ({ uploadedImage, colorPalette }) => {
           <h2>Put an image here..</h2>
         )}
       </div>
+
+      {colorPalette && (
+        <ul className={styles.colors}>
+          {colorPalette.map((color, index) => {
+            const rgb = `rgb(${color.join(",")})`;
+            const hex =
+              "#" + toHex(color[0]) + toHex(color[1]) + toHex(color[2]);
+            return <ListItem key={index} rgb={rgb} hex={hex} />;
+          })}
+        </ul>
+      )}
     </div>
   );
 };
